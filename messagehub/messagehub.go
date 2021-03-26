c537de475a27eaa5f9a2116a13936c5860e8bf22
Si Chan <si.chan@bjss.com>
2021-03-26 10:28:44 +0000

    rename messagehub structs

--- messagehub/messagehub.go
+++ messagehub/messagehub.go
@@ -1,43 +1,287 @@
 package messagehub
 
 import (
-	"sync"
+	"time"
 
 	"github.com/pkg/errors"
 )
 
-// MessageHub holds the available ChatRoom and provides thread safe access to them.
+var (
+	// ErrClosed is returned when an operation is performed on a closed MessageHub.
+	ErrClosed = errors.Errorf("cannot perform operation on closed messagehub")
+)
+
+const (
+	// EventTypeMessage is the event type for when a message is sent to the chat.
+	EventTypeMessage = "message"
+
+	// EventTypeJoin happens when a client joins the chat.
+	EventTypeJoin = "join"
+
+	// EventTypeLeave happens when a client leaves the chat.
+	EventTypeLeave = "leave"
+
+	// EventTypeClose happens when the chat is closed.
+	EventTypeClose = "close"
+)
+
+// Event is what clients in the messagehub receive when an event happens.
+// This could either be a message, someone joining or leaving the chat room.
+type Event struct {
+	ID       int
+	Type     string
+	UserID   string // UserID will be empty for Type "close"
+	Message  []byte // Message will be non-nil for Type "message"
+	UnixTime int64
+}
+
+// Request are created and sent through the MessageHub, they can be
+// intercepted with middleware.
+type Request struct {
+	Event         Event
+	eventStream   chan<- Event
+	catchUpEvents chan *eventsRingBuffer
+	done          chan struct{}
+}
+
+// MiddleWare can be attached to the MessageHub, all requests will be streamed
+// through the middleware in an orderly fashion. The channel returned by
+// the middleware should not be buffered, this ensures that the requests
+// are pulled through correctly.
+type MiddleWare func(<-chan Request) <-chan Request
+
+// MessageHub represents a chat room and handles the message passing between clients
 type MessageHub struct {
-	chatRooms map[string]*ChatRoom
+	id         string
+	clients    map[string]chan<- Event // key = userid
+	requests   chan Request
+	mids       []MiddleWare
+	eventCount int
 
-	mtx sync.RWMutex
+	// buffer holds the last n (bufferSize) events in memory
+	buffer *eventsRingBuffer
+
+	closed chan struct{}
 }
 
-// New returns an new instance of the MessageHub
-func New() *MessageHub {
-	return &MessageHub{chatRooms: make(map[string]*ChatRoom)}
-}
+// New creates a new MessageHub that is ready to accept requests.
+// The MessageHub must be given an id and a bufferSize, the bufferSize defines
+// how many events the MessageHub can hold in memory, these in memory events
+// can let new clients joining the MessageHub efficiently catchup with recent messages.
+//
+// Max bufferSize is 1024.
+// Min bufferSize is 1.
+func New(id string, bufferSize int, mids ...MiddleWare) *MessageHub {
 
-// NewChatRoom creates a new chatroom.
-func (hub *MessageHub) NewChatRoom(id string, bufferSize int) (*ChatRoom, error) {
-	hub.mtx.Lock()
-	defer hub.mtx.Unlock()
-	_, ok := hub.chatRooms[id]
-	if ok {
-		return nil, errors.Errorf("chatroom %s already exists", id)
+	// having a minimum buffer size of one means
+	// we don't have to add extra logic to handle empty
+	// buffers
+	if bufferSize < 1 {
+		bufferSize = 1
 	}
-	chatRoom := NewChatRoom(id, bufferSize)
-	hub.chatRooms[id] = chatRoom
-	return chatRoom, nil
+	if bufferSize > 1024 {
+		bufferSize = 1024
+	}
+
+	hub := &MessageHub{
+		clients:  make(map[string]chan<- Event),
+		requests: make(chan Request, bufferSize),
+		mids:     mids,
+
+		buffer: newEventsRingBuffer(bufferSize),
+
+		closed: make(chan struct{}),
+	}
+	go hub.serve()
+	return hub
 }
 
-// GetChatRoom returns returns the ChatRoom with the matching id.
-func (hub *MessageHub) GetChatRoom(id string) (*ChatRoom, error) {
-	hub.mtx.RLock()
-	defer hub.mtx.RUnlock()
-	chatRoom, ok := hub.chatRooms[id]
-	if !ok {
-		return nil, errors.Errorf("chatroom %s does not exist", id)
+// Message sends a message to the MessageHub asynchronously.
+// Sending on a closed MessageHub will be a no-op.
+func (c *MessageHub) Message(senderID string, message []byte) {
+	event := Event{
+		Type:    EventTypeMessage,
+		UserID:  senderID,
+		Message: message,
 	}
-	return chatRoom, nil
+	req := Request{Event: event}
+
+	// The messagehub server may already be closed so we
+	// must select over the channels to prevent blocking.
+	select {
+	case c.requests <- req:
+	case <-c.closed:
+	}
+}
+
+// Join adds a new user to the MessageHub. The event stream to listen on and any
+// recent events are returned.
+// The recent events are ordered from oldest to most recent.
+func (c *MessageHub) Join(userID string, streamBuffer int) (<-chan Event, []Event, error) {
+	eventStream := make(chan Event, streamBuffer)
+	event := Event{
+		Type:   EventTypeJoin,
+		UserID: userID,
+	}
+	req := Request{
+		Event:         event,
+		eventStream:   eventStream,
+		catchUpEvents: make(chan *eventsRingBuffer, 1),
+	}
+
+	select {
+	case c.requests <- req:
+	case <-c.closed:
+	}
+
+	// the messagehub server may be closed while the request is in
+	// the requests buffer and the done signal may never be received.
+	select {
+	case eventsBuf := <-req.catchUpEvents:
+
+		// we sort the buffer here rather than in the serve goroutine
+		// as we want to spend less time blocking the server.
+		return eventStream, eventsBuf.events(), nil
+	case <-c.closed:
+		return nil, nil, ErrClosed
+	}
+}
+
+// Leave removes a user from the MessageHub.
+func (c *MessageHub) Leave(userID string) error {
+	event := Event{
+		Type:   EventTypeLeave,
+		UserID: userID,
+	}
+	req := Request{
+		Event: event,
+		done:  make(chan struct{}),
+	}
+	select {
+	case c.requests <- req:
+	case <-c.closed:
+	}
+
+	select {
+	case <-req.done:
+		return nil
+	case <-c.closed:
+		return ErrClosed
+	}
+}
+
+// Close closes the the MessageHub.
+func (c *MessageHub) Close() error {
+	event := Event{
+		Type: EventTypeClose,
+	}
+	req := Request{
+		Event: event,
+		done:  make(chan struct{}),
+	}
+	select {
+	case c.requests <- req:
+	case <-c.closed:
+	}
+	select {
+	case <-req.done:
+		return nil
+	case <-c.closed:
+		return ErrClosed
+	}
+}
+
+// serve is ran in the background to serve and synchronize requests
+// through the MessageHub.
+func (c *MessageHub) serve() {
+
+	// apply generator middleware first
+	requestStream := wrapMiddleware(c.requests, c.generator)
+
+	// apply custom middleware
+	requestStream = wrapMiddleware(requestStream, c.mids...)
+
+	for req := range requestStream {
+		event := req.Event
+
+		c.buffer.push(event)
+
+		// handle request based on event type.
+		switch event.Type {
+		case EventTypeMessage:
+			c.broadcast(event)
+		case EventTypeJoin:
+			if _, ok := c.clients[event.UserID]; !ok {
+				c.clients[event.UserID] = req.eventStream
+				req.catchUpEvents <- c.buffer.makeCopy()
+				c.broadcast(event)
+				continue
+			}
+			c.clients[event.UserID] = req.eventStream
+			req.catchUpEvents <- c.buffer.makeCopy()
+		case EventTypeLeave:
+			if stream, ok := c.clients[event.UserID]; ok {
+				delete(c.clients, event.UserID)
+				close(stream)
+				close(req.done)
+				c.broadcast(event)
+				continue
+			}
+			close(req.done)
+		case EventTypeClose:
+			c.broadcast(event)
+			for userid := range c.clients {
+				delete(c.clients, userid)
+			}
+			close(c.closed)
+			return
+		}
+	}
+}
+
+// broadcast publishes the provided event to all clients in the chat.
+func (c *MessageHub) broadcast(event Event) {
+	for _, stream := range c.clients {
+		select {
+		case stream <- event:
+		default:
+			// events are dropped if no client is
+			// actively listening to their eventStream
+			// this stops the entire MessageHub server
+			// from blocking.
+		}
+	}
+}
+
+// generator is the first middleware that is applied to the request stream
+// and applies the sequential id and timestamps to events. It also closes
+// the outputted request stream once the MessageHub is closed so that other
+// middleware know when to exit.
+func (c *MessageHub) generator(in <-chan Request) <-chan Request {
+	out := make(chan Request)
+	go func() {
+		for {
+			select {
+			case req := <-in:
+				c.eventCount++
+				req.Event.ID = c.eventCount
+				req.Event.UnixTime = time.Now().Unix()
+				out <- req
+			case <-c.closed:
+				close(out)
+				return
+			}
+		}
+	}()
+	return out
+}
+
+func wrapMiddleware(requests <-chan Request, middleware ...MiddleWare) <-chan Request {
+	for i := 0; i < len(middleware); i++ {
+		mw := middleware[i]
+		if mw != nil {
+			requests = mw(requests)
+		}
+	}
+	return requests
 }
--- messagehub/messagehub_test.go
+++ messagehub/messagehub_test.go
@@ -1,44 +1,202 @@
 package messagehub
 
-import "testing"
+import (
+	"fmt"
+	"sync"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+)
 
 func TestMessageHub(t *testing.T) {
-	messageHub := New()
+	hub := New("hub-id", 10)
 
-	id1 := "1"
-	id2 := "2"
-
-	newChatRoom1, err := messageHub.NewChatRoom(id1, 100)
+	user1Events, pastEvents1, err := hub.Join("user1", 10)
 	if err != nil {
-		t.Fatalf("failed to create initial chat room id[%s]: %s", id1, err)
+		t.Fatal(err)
 	}
-
-	gotChatRoom1, err := messageHub.GetChatRoom(id1)
-	if newChatRoom1 != gotChatRoom1 {
-		t.Fatalf("should get the same chatroom for same id - id[%s]", id1)
-	}
-
-	newChatRoom2, err := messageHub.NewChatRoom(id2, 100)
+	user2Events, pastEvents2, err := hub.Join("user2", 10)
 	if err != nil {
-		t.Fatalf("failed to create initial chat room id[%s]: %s", id2, err)
+		t.Fatal(err)
+	}
+	user3Events, pastEvents3, err := hub.Join("user3", 10)
+	if err != nil {
+		t.Fatal(err)
 	}
 
-	gotChatRoom2, err := messageHub.GetChatRoom(id2)
-	if newChatRoom2 != gotChatRoom2 {
-		t.Fatalf("should get the same chatroom for same id - id[%s]", id2)
+	expectedPastEvents1 := []Event{
+		{ID: 1, Type: "join", UserID: "user1"},
+	}
+	expectedPastEvents2 := []Event{
+		{ID: 1, Type: "join", UserID: "user1"},
+		{ID: 2, Type: "join", UserID: "user2"},
+	}
+	expectedPastEvents3 := []Event{
+		{ID: 1, Type: "join", UserID: "user1"},
+		{ID: 2, Type: "join", UserID: "user2"},
+		{ID: 3, Type: "join", UserID: "user3"},
 	}
 
-	if newChatRoom1 == newChatRoom2 {
-		t.Fatalf("new chatrooms should not references other chatrooms")
+	assert.Equal(t, expectedPastEvents1, removeTime(pastEvents1), "user1 should get the correct past events when joining")
+	assert.Equal(t, expectedPastEvents2, removeTime(pastEvents2), "user2 should get the correct past events when joining")
+	assert.Equal(t, expectedPastEvents3, removeTime(pastEvents3), "user3 should get the correct past events when joining")
+
+	user1ReceivedEvents := make([]Event, 0)
+	user2ReceivedEvents := make([]Event, 0)
+	user3ReceivedEvents := make([]Event, 0)
+
+	wg := sync.WaitGroup{}
+	wg.Add(3)
+
+	go func() {
+		for event := range user1Events {
+			user1ReceivedEvents = append(user1ReceivedEvents, event)
+			if event.Type == EventTypeMessage {
+				go func() {
+					time.Sleep(20 * time.Millisecond)
+					wg.Done()
+				}()
+			}
+		}
+	}()
+
+	go func() {
+		defer wg.Done()
+		for event := range user2Events {
+			user2ReceivedEvents = append(user2ReceivedEvents, event)
+			if event.Type == EventTypeClose {
+				return
+			}
+		}
+	}()
+
+	go func() {
+		defer wg.Done()
+		for event := range user3Events {
+			user3ReceivedEvents = append(user3ReceivedEvents, event)
+			if event.Type == EventTypeClose {
+				return
+			}
+		}
+	}()
+
+	hub.Message("send-id", []byte("hello"))
+	hub.Leave("user1")
+	hub.Message("send-id", []byte("user1 left"))
+	hub.Close()
+
+	wg.Wait()
+
+	expectedEvents1 := []Event{
+		{ID: 1, Type: "join", UserID: "user1"},
+		{ID: 2, Type: "join", UserID: "user2"},
+		{ID: 3, Type: "join", UserID: "user3"},
+		{ID: 4, Type: "message", UserID: "send-id", Message: []byte("hello")},
 	}
 
-	_, err = messageHub.NewChatRoom(id1, 100)
-	if err == nil {
-		t.Fatalf("should return an error for creating a chatroom with same id")
+	expectedEvents2 := []Event{
+		{ID: 2, Type: "join", UserID: "user2"},
+		{ID: 3, Type: "join", UserID: "user3"},
+		{ID: 4, Type: "message", UserID: "send-id", Message: []byte("hello")},
+		{ID: 5, Type: "leave", UserID: "user1"},
+		{ID: 6, Type: "message", UserID: "send-id", Message: []byte("user1 left")},
+		{ID: 7, Type: "close"},
 	}
 
-	_, err = messageHub.GetChatRoom("invalid")
-	if err == nil {
-		t.Fatalf("should return an error for getting non existant chatroom")
+	expectedEvents3 := []Event{
+		{ID: 3, Type: "join", UserID: "user3"},
+		{ID: 4, Type: "message", UserID: "send-id", Message: []byte("hello")},
+		{ID: 5, Type: "leave", UserID: "user1"},
+		{ID: 6, Type: "message", UserID: "send-id", Message: []byte("user1 left")},
+		{ID: 7, Type: "close"},
 	}
+
+	assert.Equal(t, expectedEvents1, removeTime(user1ReceivedEvents), "user1 should have received the correct events")
+	assert.Equal(t, expectedEvents2, removeTime(user2ReceivedEvents), "user2 should have received the correct events")
+	assert.Equal(t, expectedEvents3, removeTime(user3ReceivedEvents), "user3 should have received the correct events")
+
+	// .Message() should be no-op on closed MessageHUb
+	hub.Message("send-id", []byte("hello"))
+
+	assert.Equal(t, 0, len(user1Events), "user1 should have 0 events in events buffer")
+	assert.Equal(t, 0, len(user2Events), "user2 should have 0 events in events buffer")
+	assert.Equal(t, 0, len(user3Events), "user3 should have 0 events in events buffer")
+
+	// Operating on closed hub should return ErrClosed.
+	_, _, err = hub.Join("user4", 10)
+	if err != ErrClosed {
+		t.Fatal("should have returned error")
+	}
+	err = hub.Leave("user2")
+	if err != ErrClosed {
+		t.Fatal("should have returned error")
+	}
+	err = hub.Close()
+	if err != ErrClosed {
+		t.Fatal("should have returned error")
+	}
+
+}
+
+func TestMessageHub_Middleware(t *testing.T) {
+
+	wg := sync.WaitGroup{}
+	wg.Add(2)
+
+	// Set up middleware which intercepts and stores the events.
+	storageEvents := make([]Event, 0)
+	storageMiddleware := func(in <-chan Request) <-chan Request {
+		out := make(chan Request)
+		go func() {
+			defer wg.Done()
+			for req := range in {
+				fmt.Printf("middleware event - %v\n", req.Event)
+				storageEvents = append(storageEvents, req.Event)
+				out <- req
+			}
+		}()
+		return out
+	}
+
+	hub := New("hub-id", 10, storageMiddleware)
+
+	userEvents, _, err := hub.Join("user1", 10)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	userReceivedEvents := make([]Event, 0)
+
+	go func() {
+		defer wg.Done()
+		for event := range userEvents {
+			fmt.Printf("user event - %v\n", event)
+			userReceivedEvents = append(userReceivedEvents, event)
+			if event.Type == EventTypeClose {
+				return
+			}
+		}
+	}()
+
+	hub.Message("send-id", []byte("message1"))
+	hub.Message("send-id", []byte("message2"))
+	hub.Message("send-id", []byte("message3"))
+	hub.Close()
+
+	wg.Wait()
+
+	assert.Equal(t, storageEvents, userReceivedEvents, "middleware events should be equal to user received events")
+
+}
+
+// removeTime removes the timestamp for comparison as this will be non-deterministic.
+func removeTime(events []Event) []Event {
+	modEvents := make([]Event, len(events))
+	for i := range events {
+		event := events[i]
+		event.UnixTime = 0
+		modEvents[i] = event
+	}
+	return modEvents
 }
